d54b4f2212e77059d64cce4b8597dc5b56215caf
Bidyut Kumar Patra <SESA503898@schneider-electric.com>
2020-11-18 18:46:27 +0530

    changes

--- process_injection/target/App.xaml.cs
+++ process_injection/target/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,36 @@ namespace target
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            this.WatchThreads();
+        }
+
+        private void WatchThreads()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    var threads = Process.GetCurrentProcess().Threads;
+                    var threadList = new ObservableCollection<ThreadInfo>();
+                    for(var i = 0; i < threads.Count; i++)
+                    {
+                        var thread = threads[i];
+                        var threadInfo = new ThreadInfo()
+                        {
+                            Identifier = thread.Id,
+                            Thread = thread
+                        };
+                        threadList.Add(threadInfo);
+                    }
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        MainViewModel.Instance.Threads = threadList;
+                    });
+                }
+            });
+        }
     }
 }
--- process_injection/target/MainWindow.xaml.cs
+++ process_injection/target/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,42 @@ namespace target
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = MainViewModel.Instance;
+        }
+    }
+
+    public class MainViewModel
+    {
+        public ObservableCollection<ThreadInfo> Threads { get; set; }
+
+        private static MainViewModel _instance;
+        public static MainViewModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new MainViewModel();
+                }
+                return _instance;
+            }
+        }
+
+        private MainViewModel()
+        {
+            this.Threads = new ObservableCollection<ThreadInfo>();
+        }
+    }
+
+    public class ThreadInfo
+    {
+        public string Name { get; set; }
+        public int Identifier { get; set; }
+        public ProcessThread Thread { get; set; }
+
+        public ThreadInfo()
+        {
+            this.Name = "Default";
         }
     }
 }
